97fe3a73f3ac1164a4d3ea97dbb30f566603f41a
Zakaria96 <zakarialavoro@gmail.com>
2023-04-27 14:43:43 +0200

    struttura appunti js

---
+++ js/biglietto-treno.js
@@ -0,0 +1,16 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero. Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole: il prezzo del biglietto è definito in base ai km (0.21 € al km) va applicato uno sconto del 20% per i minorenni va applicato uno sconto del 40% per gli over 65. L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+// VARIABILI GLOBALI 
+
+// chiedere all’utente il numero di chilometri che vuole percorrere
+let kmPercorso = parseInt(prompt("Quanti kilometri intende percorere?"))
+console.log(kmPercorso);
+// chiedere all’utente l’età del passeggero
+
+// calcolare il prezzo totale del viaggio
+
+// applicare uno sconto del 20% per i minorenni
+
+// applicare uno sconto del 40% per gli over 65
+
+// L’output del prezzo finale 
